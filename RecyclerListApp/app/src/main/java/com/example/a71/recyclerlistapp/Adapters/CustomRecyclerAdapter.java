c582306fa5e952b0a17ffc0922ef08f1d06e1079
mapis1512 <mariapaulagp@hotmail.com>
2017-07-09 08:28:20 -0500

    recycler

---
+++ RecyclerListApp/app/src/main/java/com/example/a71/recyclerlistapp/Adapters/CustomRecyclerAdapter.java
@@ -0,0 +1,61 @@
+package com.example.a71.recyclerlistapp.Adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.a71.recyclerlistapp.R;
+
+/**
+ * Created by 71 on 7/9/2017.
+ */
+
+public class CustomRecyclerAdapter extends RecyclerView.Adapter<CustomRecyclerAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public ViewHolder(View v) {
+            super(v);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public CustomRecyclerAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public CustomRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.customrecycler, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
---
+++ RecyclerListApp/app/src/main/java/com/example/a71/recyclerlistapp/Fragments/ListaFragment.java
@@ -0,0 +1,26 @@
+package com.example.a71.recyclerlistapp.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.a71.recyclerlistapp.R;
+
+/**
+ * Created by 71 on 7/9/2017.
+ */
+
+public class ListaFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.lista_fragment, container, false);
+        RecyclerView recyclerview = (RecyclerView) view.findViewById(R.id.recycler);
+
+        return view;
+    }
+}
